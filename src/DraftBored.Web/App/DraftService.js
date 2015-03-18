8447a04b01a4f4261eecdba32b285d60b4a7900c
Heath <heath@logic-bomb.com>
2015-03-18 18:48:44 -0400

    Separated start/stop sim buttons and added timer\loop for sim

--- src/DraftBored.Web/App/DraftService.js
+++ src/DraftBored.Web/App/DraftService.js
@@ -2,12 +2,12 @@
 	/**
 	* @ngInject
 	*/
-	function DraftService($q, DataService, DraftSettings) {
+	function DraftService(DataService, DraftSettings) {
 		var svc = this;
 		svc.draft = DraftSettings;
 		svc.draft.drafted = [];
-		svc.GetTeam = function (shortName, teams) {
-			return teams.filter(function (t) {
+		svc.GetTeam = function (shortName) {
+			return svc.draft.teams.filter(function (t) {
 				return t.short_name === shortName.toUpperCase();
 			})[0];
 		};
@@ -21,7 +21,7 @@
 				DataService
 					.GetDraft(year)
 					.success(function (data, status, headers, config) {
-						var team = svc.GetTeam(shortName, data.teams);
+						var team = svc.GetTeam(shortName);
 						svc.draft = DraftSettings;
 						svc.draft.load(team, data);
 						success();
@@ -45,6 +45,11 @@
 			svc.draft.drafted.push(prospect);
 			svc.draft.picks.splice(0, 1);
 			svc.draft.round = svc.draft.picks[0].round_num;
+		};
+
+		svc.SimulatePick = function () {
+			var topPick = svc.draft.prospects[0];
+			svc.SelectProspect(topPick);
 		};
 	}
 
--- src/DraftBored.Web/App/Sim/Sim.js
+++ src/DraftBored.Web/App/Sim/Sim.js
@@ -2,10 +2,11 @@
 	/**
 	* @ngInject
 	*/
-	function Sim($routeParams, DataService, DraftService) {
+	function Sim($routeParams, $interval, DraftService) {
 		var vm = this;
 		var DraftedListMininimum = 5;
 		var teamId = $routeParams.team.toUpperCase();
+		var loopPromise = {};
 
 		var setDraft = function () {
 			vm.draft = DraftService.draft;
@@ -13,9 +14,27 @@
 		
 		DraftService.PrepareDraft(teamId, 2015, setDraft);
 
+		vm.loop = function () {
+			DraftService.SimulatePick();
+		};
+
 		vm.selectProspect = function (prospect) {
 			console.log(prospect.name);
 			DraftService.SelectProspect(prospect);
+		};
+
+		vm.startSim = function () {
+			if (!vm.draft.isInProgress()) {
+				vm.draft.start();
+				loopPromise = $interval(vm.loop, 1000, vm.draft.prospects.length);
+			}
+		};
+
+		vm.pauseSim = function () {
+			if (vm.draft.isInProgress()) {
+				vm.draft.stop();
+				$interval.cancel(loopPromise);
+			}
 		};
 	}
 
